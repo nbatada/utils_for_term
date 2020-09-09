14c46b85fc6afb84572d2b6a29ae92f9376c27b0
Nizar Batada <nizarb@23andme.com>
2020-09-08 23:43:26 -0700

    type

--- field_move.py
+++ field_move.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
-# updated (corrected) it on 10 March 2020
-# Sun Aug 16 20:49:58 2015 has a bug
+#  10 March 2020
+# Nizar Batada nizar.batada "_at_" gmail.com
+
 from __future__ import print_function # this is for print of v3 in v2.7
+
+'''
+USAGE:
+To move column 1 to column 4, do
+
+cat myfile.tsv | field_move.py -i 1 -j 4
+
+
+'''
 import os,sys
 from sys import argv,exit,stdin,stderr,path
 import argparse
@@ -11,12 +21,14 @@ def error(str):
     exit(-1)
     
 if __name__ == '__main__':
-    parser=argparse.ArgumentParser(description='This program ...')
-    parser.add_argument('-f','--file',default=None,action='store',nargs=1,required=False,help='File name to read from')
-    parser.add_argument('-i','--idx_from',type=int,required=True,help='index of the column to move')
-    parser.add_argument('-j','--idx_to',type=int,required=True,help='index of the column to move to')
+    parser=argparse.ArgumentParser(description='This program allows you to move around specific columns in a tab delimited file')
+    parser.add_argument('-f','--file',default=None,action='store',nargs=1,required=False,help='File name to read from [Default: stdin]')
+    parser.add_argument('-i','--idx_from',type=int,required=True,help='index of the column to move. 1-based index.')
+    parser.add_argument('-j','--idx_to',type=int,required=True,help='index of the column to move to'. 1-based index.)
+    parser.add_argumen('-s','--sep',default='\t',action='store',required=False, help='delimiter separating columns [Default: \t]')
     args=parser.parse_args()
-    sep='\t'
+    if args.sep=='\\t' or args.sep=='tab':
+        sep='\t'
     if args.idx_from <=0 or args.idx_to<=0:
         error("Error: idx_from and idx_to must be greater than 0.")
     src=args.idx_from
